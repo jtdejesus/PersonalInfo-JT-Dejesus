ccc3bb33733e1e509a9b23e7cba98ab9646fb5f7
jtdejesus <157234255+jtdejesus@users.noreply.github.com>
2024-05-25 00:24:39 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    var fullName = ("Robert Hall");
+    var DateofBirth = ("2/27/1949");
+    var workPhone = ("work 000-444-0101");
+    var cellPhone = ("cell 000-444-0189");
+    Console.WriteLine(fullName);
+    Console.WriteLine(DateofBirth);
+    Console.WriteLine(workPhone);
+    Console.WriteLine(cellPhone);
+  }
+
+  
+    
+}
